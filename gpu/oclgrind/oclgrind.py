99eb8e3ac79099e923ca5d2e06376a6c3a95070a
Chris Cummins <chrisc.101@gmail.com>
2018-06-20 11:20:49 +0000

    Add a python interface to oclgrind.
    [Exported from 5e1e4f5198d4252841b96504ba4878b4770140f2]

--- gpu/oclgrind/oclgrind.py
+++ gpu/oclgrind/oclgrind.py
@@ -1,16 +1,3 @@
-# Copyright 2018-2020 Chris Cummins <chrisc.101@gmail.com>.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """A python wrapper around oclgrind, an OpenCL device simulator and debugger.
 
 Oclgrind is developed by James Price <j.price@bristol.ac.uk>.
@@ -22,38 +9,32 @@ use '--' to prevent this script from attempting to parse the args, and a second
 
 Usage:
 
-  bazel run //gpu/oclgrind [-- -- <args>]
+  bazel run //gpu/oclgrind -- -- --version
 """
 import subprocess
 import sys
 import typing
 
-from gpu.clinfo.proto import clinfo_pb2
-from labm8.py import app
-from labm8.py import bazelutil
+from absl import app
+from absl import flags
+from absl import logging
 
-FLAGS = app.FLAGS
-
-OCLGRIND_PATH = bazelutil.DataPath("phd/third_party/oclgrind/oclgrind")
-
-# The clinfo description of the local Oclgrind binary.
-CLINFO_DESCRIPTION = clinfo_pb2.OpenClDevice(
-  name="Emulator|Oclgrind|Oclgrind_Simulator|Oclgrind_18.3|1.2",
-  platform_name="Oclgrind",
-  device_name="Oclgrind Simulator",
-  driver_version="Oclgrind 18.3",
-  opencl_version="1.2",
-  device_type="Emulator",
-  platform_id=0,
-  device_id=0,
-)
+from lib.labm8 import bazelutil
+from lib.labm8 import system
 
 
-def Exec(
-  argv: typing.List[str],
-  stdin: typing.Optional[str] = None,
-  env: typing.Dict[str, str] = None,
-) -> subprocess.Popen:
+FLAGS = flags.FLAGS
+
+# The path to the oclgrind binary.
+_OCLGRIND_PKG = 'oclgrind_linux' if system.is_linux() else 'oclgrind_mac'
+OCLGRIND_PATH = bazelutil.DataPath(f'{_OCLGRIND_PKG}/bin/oclgrind')
+
+# The name of the oclgrind device, as returned by
+# gpu.cldrive.env.OpenClEnvironment.name().
+OCLGRIND_CLDRIVE_NAME = "Emulator|Oclgrind|Oclgrind_Simulator|Oclgrind_18.3|1.2"
+
+
+def Exec(argv: typing.List[str]) -> subprocess.Popen:
   """Execute a command using oclgrind.
 
   This creates a Popen process, executes it, and sets the stdout and stderr
@@ -61,25 +42,15 @@ def Exec(
 
   Args:
     argv: A list of arguments to pass to the oclgrind executable.
-    env: An optional environment to use.
 
   Returns:
     A Popen instance, with string stdout and stderr attributes set.
   """
   cmd = [str(OCLGRIND_PATH)] + argv
-  app.Log(3, "$ %s", " ".join(cmd))
-  process = subprocess.Popen(
-    cmd,
-    stdout=subprocess.PIPE,
-    stdin=subprocess.PIPE if stdin else None,
-    stderr=subprocess.PIPE,
-    universal_newlines=True,
-    env=env,
-  )
-  if stdin:
-    stdout, stderr = process.communicate(stdin)
-  else:
-    stdout, stderr = process.communicate()
+  logging.debug('$ %s', ' '.join(cmd))
+  process = subprocess.Popen(cmd, stdout=subprocess.PIPE,
+                             stderr=subprocess.PIPE, universal_newlines=True)
+  stdout, stderr = process.communicate()
   process.stdout, process.stderr = stdout, stderr
   return process
 
@@ -92,5 +63,5 @@ def main(argv):
   sys.exit(proc.returncode)
 
 
-if __name__ == "__main__":
-  app.RunWithArgs(main)
+if __name__ == '__main__':
+  app.run(main)
