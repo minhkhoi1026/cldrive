1003eefa73bf7ed2a9e955d29738a591aa96ae1f
Chris Cummins <chrisc.101@gmail.com>
2018-06-26 01:17:16 +0000

    Split clinfo implementation into library.
    [Exported from 738ca7e1878567e3e83d68b78c1fdd19b442819d]

--- gpu/clinfo/libclinfo.h
+++ gpu/clinfo/libclinfo.h
@@ -1,78 +1,37 @@
 // Get information about available OpenCL devices.
-//
-// Copyright 2017-2020 Chris Cummins <chrisc.101@gmail.com>.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
 #ifndef PHD_GPU_LIBCLINFO_H
 #define PHD_GPU_LIBCLINFO_H
 
-#include "third_party/opencl/cl.hpp"
+#include <vector>
+
+// Enable OpenCL cl::Error class and exceptions.
+// This must be defined before including the OpenCL header.
+#define __CL_ENABLE_EXCEPTIONS
+
+#include "third_party/opencl/include/cl.hpp"
 
 #include "gpu/clinfo/proto/clinfo.pb.h"
-#include "labm8/cpp/statusor.h"
-#include "labm8/cpp/string.h"
 
-// TODO(cec): Refactor to remove labm8:: namespace.
-namespace labm8 {
+namespace phd {
 
 namespace gpu {
 
 namespace clinfo {
 
-// TODO(cec): Refactor so that each returns StatusOr, not exception.
+const char *OpenClErrorString(cl_int err);
 
-const char* OpenClErrorString(cl_int err);
+void OpenClCheckError(const char *api_call, cl_int err);
 
-void OpenClCheckError(const string& api_call, cl_int err);
-
-void SetOpenClDevice(const cl::Platform& platform, const cl::Device& device,
-                     const int platform_id, const int device_id,
-                     ::gpu::clinfo::OpenClDevice* const message);
+void SetOpenClDevice(const cl::Platform &platform, const cl::Device &device,
+                     ::gpu::clinfo::OpenClDevice *const message);
 
 ::gpu::clinfo::OpenClDevices GetOpenClDevices();
-
-::gpu::clinfo::OpenClDevice GetOpenClDevice(const int platform_id,
-                                            const int device_id);
-
-// Find and return the OpenCL device proto by name.
-StatusOr<::gpu::clinfo::OpenClDevice> GetOpenClDeviceProto(const string& name);
-
-// Lookup an OpenCL device by its proto representation. Raises
-// std::invalid_argument if not found.
-cl::Device GetOpenClDevice(const ::gpu::clinfo::OpenClDevice& device);
-
-// Return a "default" OpenCL device, where "default" will attempt to pick some
-// safe device. If available, Oclgrind will be used. Else, the first device
-// as it appears in the enumerated list.
-// TODO(cec): It would be nice if we could link against oclgrind to ensure that
-// this function could always return the oclgrind device.
-cl::Device GetDefaultOpenClDeviceOrDie();
-
-// Same as above, but abort if device is not available.
-cl::Device GetOpenClDeviceOrDie(const ::gpu::clinfo::OpenClDevice& device);
-
-// Lookup an OpenCL device by its name. Raises std::invalid_argument if not
-// found.
-cl::Device GetOpenClDevice(const string& name);
-
-// Same as above, but abort if device is not available.
-cl::Device GetOpenClDeviceOrDie(const string& name);
 
 }  // namespace clinfo
 
 }  // namespace gpu
 
-}  // namespace labm8
+}  // namespace phd
 
-#endif  // PHD_GPU_LIBCLINFO_H
+#endif //PHD_GPU_LIBCLINFO_H
