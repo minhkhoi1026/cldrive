79964e83e5c1a267c6e68a8b32d84a3db277954c
Chris Cummins <chrisc.101@gmail.com>
2018-12-02 03:05:45 +0000

    Add a strings header.
    [Exported from 1339f8b78554cd80d3992453fb2fdb24ee2a7e57]

--- phd/string.h
+++ phd/string.h
@@ -1,44 +1,57 @@
 // Utility code for working with strings.
 #pragma once
 
-#include <string>
-
 using std::string;
 
 namespace phd {
 
-// Trim leading and trailing whitespace from a string.
-string &Trim(string &s);
-
-// Trim leading and trailing whitespace from a string.
-string CopyAndTrim(string s);
-
-// Trim leading whitespace from a string.
-void TrimLeft(string &s);
-
-// Trim a string from the left.
-string CopyAndTrimLeft(string s);
+// Trim a string from the left in-place.
+void TrimLeft(string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](int ch) {
+        return !std::isspace(ch);
+    }));
+}
 
 // Trim a string from the end in-place.
-void TrimRight(string &s);
+void TrimRight(string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](int ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
+}
+
+// Trim a string from both ends in-place.
+string& Trim(string &s) {
+    TrimLeft(s);
+    TrimRight(s);
+    return s;
+}
+
+// Trim a string from the left.
+string TrimLeftCopy(string s) {
+    TrimLeft(s);
+    return s;
+}
 
 // Trim a string from the right.
-string CopyAndTrimRight(string s);
+string TrimRightCopy(string s) {
+    TrimRight(s);
+    return s;
+}
+
+// Trim a string from both ends.
+string TrimCopy(string s) {
+    Trim(s);
+    return s;
+}
 
 // Returns whether full_string ends with suffix.
-bool EndsWith(const string &full_string, const string &suffix);
+bool EndsWith(const string& full_string, const string& suffix) {
+  if (full_string.length() >= suffix.length()) {
+    return full_string.compare(
+        full_string.length() - suffix.length(), suffix.length(), suffix) == 0;
+  } else {
+    return false;
+  }
+}
 
-// Split a string on whitespace and convert the first letter of each wort
-// to CamelCase. E.g. "hello world" -> "HelloWorld".
-string ToCamelCase(const string &full_string);
-
-string ReplaceChar(string &s, const char src, const char dst);
-
-string CopyAndReplaceChar(const string &s, const char src, const char dst);
-
-string ReplaceSubstr(string &s, const string &src, const string &dst);
-
-string CopyAndReplaceSubstr(const string &s, const string &src,
-                            const string &dst);
-
-} // namespace phd
+}  // namespace phd
