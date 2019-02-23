d5f41a8b91f9f794471211cff62b39a386eeb217
Chris Cummins <chrisc.101@gmail.com>
2019-02-23 13:13:51 +0000

    Rename native_driver_lib to libcldrive.
    [Exported from 9309c2b6156a32a6d6246bcc8dfa98d91c3382ef]

--- gpu/cldrive/native_driver.cc
+++ gpu/cldrive/native_driver.cc
@@ -1,45 +1,7 @@
-<<<<<<< HEAD
-// Copyright (c) 2016, 2017, 2018, 2019 Chris Cummins.
-// This file is part of cldrive.
-//
-// cldrive is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// cldrive is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with cldrive.  If not, see <https://www.gnu.org/licenses/>.
 #include "gpu/cldrive/libcldrive.h"
-
-#include "phd/pbutil.h"
-
-namespace gpu {
-namespace cldrive {
-
-void ProcessCldriveInstancesOrDie(CldriveInstances* instances) {
-  ProtocolBufferLogger logger(std::cout, instances, /*text_format=*/false);
-  for (int i = 0; i < instances->instance_size(); ++i) {
-    logger.StartNewInstance();
-    Cldrive(instances->mutable_instance(i), i).RunOrDie(logger);
-  }
-}
-
-}  // namespace cldrive
-}  // namespace gpu
-
-PBUTIL_INPLACE_PROCESS_MAIN(gpu::cldrive::ProcessCldriveInstancesOrDie,
-                            gpu::cldrive::CldriveInstances);
-=======
-#include "gpu/cldrive/native_driver_lib.h"
 #include "gpu/cldrive/proto/cldrive.pb.h"
 
 #include "phd/pbutil.h"
 
 PBUTIL_INPLACE_PROCESS_MAIN(gpu::cldrive::ProcessCldriveInstanceOrDie,
                             gpu::cldrive::CldriveInstance);
->>>>>>> c0eda32ea... Begin native implementation of cldrive.
