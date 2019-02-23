24f01ab10e5f5ba19f8f5501bd5c038c997ee04f
Chris Cummins <chrisc.101@gmail.com>
2019-02-23 13:13:51 +0000

    Rename native_driver_lib to libcldrive.
    [Exported from 9309c2b6156a32a6d6246bcc8dfa98d91c3382ef]

--- gpu/cldrive/libcldrive.h
+++ gpu/cldrive/libcldrive.h
@@ -1,43 +1,13 @@
-// Copyright (c) 2016, 2017, 2018, 2019 Chris Cummins.
-// This file is part of cldrive.
+// TODO(cec):
 //
-// cldrive is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// cldrive is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with cldrive.  If not, see <https://www.gnu.org/licenses/>.
 #pragma once
 
-#include "gpu/cldrive/logger.h"
 #include "gpu/cldrive/proto/cldrive.pb.h"
-
-#include "third_party/opencl/cl.hpp"
 
 namespace gpu {
 namespace cldrive {
 
-class Cldrive {
- public:
-  Cldrive(CldriveInstance* instance, int instance_num = 0);
-
-  void RunOrDie(Logger& logger);
-
- private:
-  void DoRunOrDie(Logger& logger);
-
-  CldriveInstance* instance_;
-  int instance_num_;
-  cl::Device device_;
-};
-
-void ProcessCldriveInstancesOrDie(CldriveInstances* instance);
+void ProcessCldriveInstanceOrDie(CldriveInstance *instance);
 
 }  // namespace cldrive
 }  // namespace gpu
