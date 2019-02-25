268f44013a7764ccb9ac562a330552682a3ddb7a
Chris Cummins <chrisc.101@gmail.com>
2019-02-25 18:08:18 +0000

    Add C++ test util.
    [Exported from 201ef62e361db2555bafafa63fd06de3448e635d]

--- gpu/cldrive/testutil.cc
+++ gpu/cldrive/testutil.cc
@@ -1,18 +1,3 @@
-// Copyright (c) 2016, 2017, 2018, 2019 Chris Cummins.
-// This file is part of cldrive.
-//
-// cldrive is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// cldrive is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with cldrive.  If not, see <https://www.gnu.org/licenses/>.
 #include "gpu/cldrive/testutil.h"
 
 namespace gpu {
@@ -29,10 +14,10 @@ cl::Kernel CreateClKernel(const string& opencl_kernel) {
   return kernels[0];
 }
 
-DynamicParams MakeParams(size_t global_size, size_t local_size) {
+DynamicParams MakeParams(size_t global_size, size_t local_size = 1) {
   DynamicParams params;
   params.set_global_size_x(global_size);
-  params.set_local_size_x(local_size);
+  params.set_global_size_x(local_size);
   return params;
 }
 
