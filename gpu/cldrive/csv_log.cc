5c62251f8b6ed0e2e8512ebd3c7241950e347c2b
Chris Cummins <chrisc.101@gmail.com>
2019-02-28 14:16:20 +0000

    Begin work on CSV logging.
    [Exported from f42b1a9bf7a4709332cf333f417f78c91c109f54]

--- gpu/cldrive/csv_log.cc
+++ gpu/cldrive/csv_log.cc
@@ -1,18 +1,3 @@
-// Copyright (c) 2016, 2017, 2018, 2019 Chris Cummins.
-// This file is part of cldrive.
-//
-// cldrive is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// cldrive is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with cldrive.  If not, see <https://www.gnu.org/licenses/>.
 #include "gpu/cldrive/csv_log.h"
 
 #include "phd/logging.h"
@@ -22,98 +7,72 @@
 namespace gpu {
 namespace cldrive {
 
-// Print a value only if it is not zero.
 template <typename T>
 std::ostream& NullIfZero(std::ostream& stream, const T& value) {
   if (value != 0) {
     stream << value;
   }
+  stream << ",";
   return stream;
 }
 
-// Print a value only if it is not empty().
 template <typename T>
 std::ostream& NullIfEmpty(std::ostream& stream, const T& value) {
   if (!value.empty()) {
     stream << value;
   }
-  return stream;
-}
-
-// Print a value only if it is not less than zero.
-template <typename T>
-std::ostream& NullIfNegative(std::ostream& stream, const T& value) {
-  if (value >= 0) {
-    stream << value;
-  }
+  stream << ",";
   return stream;
 }
 
 std::ostream& operator<<(std::ostream& stream, const CsvLogHeader& header) {
   stream << "instance,device,build_opts,kernel,work_item_local_mem_size,"
          << "work_item_private_mem_size,global_size,local_size,outcome,"
-         << "transferred_bytes,transfer_time_ns,kernel_time_ns\n";
+         << "runtime_ns,transferred_bytes";
   return stream;
-}
-
-CsvLog::CsvLog(int instance_id)
-    : instance_id_(instance_id),
-      work_item_local_mem_size_(-1),
-      work_item_private_mem_size_(-1),
-      global_size_(-1),
-      local_size_(-1),
-      transferred_bytes_(-1),
-      transfer_time_ns_(-1),
-      kernel_time_ns_(-1) {
-  CHECK(instance_id >= 0) << "Negative instance ID not allowed";
 }
 
 std::ostream& operator<<(std::ostream& stream, const CsvLog& log) {
-  stream << log.instance_id_ << "," << log.device_ << "," << log.build_opts_
+  stream << log.instance_id << "," << log.device << "," << log.build_opts
          << ",";
-  NullIfEmpty(stream, log.kernel_) << ",";
-  NullIfNegative(stream, log.work_item_local_mem_size_) << ",";
-  NullIfNegative(stream, log.work_item_private_mem_size_) << ",";
-  NullIfNegative(stream, log.global_size_) << ",";
-  NullIfNegative(stream, log.local_size_) << "," << log.outcome_ << ",";
-  NullIfNegative(stream, log.transferred_bytes_) << ",";
-  NullIfNegative(stream, log.transfer_time_ns_) << ",";
-  NullIfNegative(stream, log.kernel_time_ns_) << std::endl;
+  NullIfEmpty(stream, log.kernel) << ",";
+  NullIfZero(stream, log.work_item_local_mem_size);
+  NullIfZero(stream, log.work_item_private_mem_size);
+  NullIfZero(stream, log.global_size);
+  NullIfZero(stream, log.local_size) << log.outcome << ",";
+  NullIfZero(stream, log.runtime_ns);
+  NullIfZero(stream, log.transferred_bytes) << std::endl;
   return stream;
 }
 
-/*static*/ CsvLog CsvLog::FromProtos(
-    int instance_id, const CldriveInstance* const instance,
-    const CldriveKernelInstance* const kernel_instance,
-    const CldriveKernelRun* const run,
-    const gpu::libcecl::OpenClKernelInvocation* const log) {
-  CsvLog csv(instance_id);
+static CsvLog FromProtos(int instance_id, CldriveInstance* instance,
+                         CldriveKernelInstance* kernel_instance,
+                         CldriveKernelRun* run,
+                         gpu::libcecl::OpenClKernelInvocation* log) {
+  CsvLog csv;
+  csv.instance_id = instance_id;
 
-  CHECK(instance) << "CldriveInstance pointer cannot be null";
-  csv.device_ = instance->device().name();
-  csv.build_opts_ = instance->build_opts();
+  CHECK(instance);
+  csv.device = instance->device().name();
+  csv.build_opts = instance->build_opts();
 
-  csv.outcome_ = CldriveInstance::InstanceOutcome_Name(instance->outcome());
+  csv.outcome = CldriveInstance::InstanceOutcome_Name(instance->outcome());
   if (kernel_instance) {
-    csv.kernel_ = kernel_instance->name();
-    csv.work_item_local_mem_size_ =
+    csv.kernel = kernel_instance->name();
+    csv.work_item_local_mem_size =
         kernel_instance->work_item_local_mem_size_in_bytes();
-    csv.work_item_private_mem_size_ =
+    csv.work_item_private_mem_size =
         kernel_instance->work_item_private_mem_size_in_bytes();
 
-    csv.outcome_ = CldriveKernelInstance::KernelInstanceOutcome_Name(
+    csv.outcome = CldriveKernelInstance::KernelInstanceOutcome_Name(
         kernel_instance->outcome());
     if (run) {
-      csv.outcome_ = CldriveKernelRun::KernelRunOutcome_Name(run->outcome());
+      csv.outcome = CldriveKernelRun::KernelRunOutcome_Name(run->outcome());
       if (log) {
-        csv.global_size_ = log->global_size();
-        csv.local_size_ = log->local_size();
-        if (log->transferred_bytes() >= 0) {
-          csv.outcome_ = "PASS";
-          csv.kernel_time_ns_ = log->kernel_time_ns();
-          csv.transfer_time_ns_ = log->transfer_time_ns();
-          csv.transferred_bytes_ = log->transferred_bytes();
-        }
+        csv.global_size = log->global_size();
+        csv.local_size = log->local_size();
+        csv.runtime_ms = log->runtime_ms();
+        csv.transferred_bytes = log->transferred_bytes();
       }
     }
   }
--- gpu/cldrive/csv_log.h
+++ gpu/cldrive/csv_log.h
@@ -1,18 +1,3 @@
-// Copyright (c) 2016, 2017, 2018, 2019 Chris Cummins.
-// This file is part of cldrive.
-//
-// cldrive is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// cldrive is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with cldrive.  If not, see <https://www.gnu.org/licenses/>.
 #pragma once
 
 #include "gpu/cldrive/proto/cldrive.pb.h"
@@ -24,81 +9,65 @@
 namespace gpu {
 namespace cldrive {
 
-// A class which prints the header values for a CSV row.
-//
-// Usage:
-//    std::cout << CsvLogHeader();
 class CsvLogHeader {
-  // Format CSV header to output stream.
   friend std::ostream& operator<<(std::ostream& stream,
                                   const CsvLogHeader& log);
 };
 
-// A class which formats a CSV row.
-//
-// Usage:
-//    CsvLog::FromProtos log(...);
-//    std::cout << log;
 class CsvLog {
  public:
-  CsvLog(int instance_id);
-
-  // Create a log from proto messages.
-  static CsvLog FromProtos(
-      int instance_id, const CldriveInstance* const instance,
-      const CldriveKernelInstance* const kernel_instance,
-      const CldriveKernelRun* const run,
-      const gpu::libcecl::OpenClKernelInvocation* const log);
-
-  // Format CSV to output stream.
-  friend std::ostream& operator<<(std::ostream& stream, const CsvLog& log);
-
- private:
-  // Begin CSV columns (in order) -----------------------------------
+  CsvLog() = default;
 
   // From CldriveInstance.opencl_src field.
-  int instance_id_;
+  int instance_id;
 
   // From CldriveInstance.device.name field.
-  string device_;
+  string device;
 
   // From CldriveInstance.build_opts field.
-  string build_opts_;
+  string build_opts;
 
   // The OpenCL kernel name, from CldriveKernelInstance.name
   // field. If CldriveInstance.outcome != PASS, this will be empty.
-  string kernel_;
+  string kernel;
 
   // From CldriveKernelInstance.work_item_local_mem_size_in_bytes field. If
   // CldriveInstance.outcome != PASS, this will be empty.
-  int work_item_local_mem_size_;
+  int work_item_local_mem_size;
 
   // From CldriveKernelInstance.work_item_private_mem_size_in_bytes field. If
   // CldriveInstance.outcome != PASS, this will be empty.
-  int work_item_private_mem_size_;
+  int work_item_private_mem_size;
 
   // From CldriveInstance.dynamic_params.global_size_x field. If
   // CldriveInstance.outcome != PASS, this will be empty.
-  int global_size_;
+  int global_size;
 
   // From CldriveInstance.dynamic_params.local_size_x field. If
   // CldriveInstance.outcome != PASS, this will be empty.
-  int local_size_;
+  int local_size;
 
   // A stringified enum value. Either CldriveInstance.outcome if
   // CldriveInstance.outcome != PASS, else CldriveKernelInstance.outcome if
   // CldriveKernelInstance.outcome != PASS, else CldriveKernelRun.outcome.
-  string outcome_;
+  string outcome;
 
-  // From CldriveKernelRun.log. If outcome != PASS, these will be empty.
-  phd::int64 transferred_bytes_;
-  phd::int64 transfer_time_ns_;
-  phd::int64 kernel_time_ns_;
+  // From CldriveKernelRun.log.runtime_ms. If outcome != PASS, this will be
+  // empty.
+  double runtime_ms;
 
-  // End CSV columns (in order) -----------------------------------
+  // From CldriveKernelRun.log.transferred_bytes. If outcome != PASS, this will
+  // be empty.
+  phd::int64 transferred_bytes;
+
+  static CsvLog FromProtos(int instance_id, CldriveInstance* instance,
+                           CldriveKernelInstance* kernel_instance,
+                           CldriveKernelRun* run,
+                           gpu::libcecl::OpenClKernelInvocation* log);
+
+  friend std::ostream& operator<<(std::ostream& stream, const CsvLog& log);
 };
 
-//
 std::ostream& operator<<(std::ostream& stream, const CsvLogHeader& log);
 std::ostream& operator<<(std::ostream& stream, const CsvLog& log);
 
