8f2905df580d5dce904555770f69fd19fd333c60
Chris Cummins <chrisc.101@gmail.com>
2019-03-06 12:32:32 +0000

    Work in progress on cldrive.
    [Exported from e653ad12a893b61740cc1176202b83ef57b02ff6]

--- gpu/cldrive/native_csv_driver.cc
+++ gpu/cldrive/native_csv_driver.cc
@@ -13,63 +13,26 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with cldrive.  If not, see <https://www.gnu.org/licenses/>.
-<<<<<<< HEAD:gpu/cldrive/native_csv_driver.cc
 #include "gpu/cldrive/libcldrive.h"
-=======
-#include "gpu/cldrive/array_kernel_arg_value.h"
-
-#include "gpu/cldrive/kernel_arg_value.h"
-
-#include "third_party/opencl/cl.hpp"
->>>>>>> 10c24c393... Move opencl headers into top dir.:gpu/cldrive/array_kernel_arg_value.cc
 
 #include "phd/logging.h"
 
 namespace gpu {
 namespace cldrive {
 
-<<<<<<< HEAD:gpu/cldrive/native_csv_driver.cc
-<<<<<<< HEAD:gpu/cldrive/native_csv_driver.cc
 void ProcessCldriveInstancesOrDie(CldriveInstances* instances) {
   CsvLogger logger(std::cout, instances);
   for (int i = 0; i < instances->instance_size(); ++i) {
     logger.StartNewInstance();
     Cldrive(instances->mutable_instance(i), i).RunOrDie(logger);
   }
-=======
-namespace {
-
-// TODO(cldrive): Work in progress!
-
-template <typename T>
-bool Vec2Equality(const T& lhs, const T& rhs) {
-  return true;
-  // TODO: return (l[0] == r[0]) && (l[1] == r[1]);
 }
-
-}  // anonymous namespace
-
-template <>
-/*virtual*/ string ArrayKernelArgValue<cl_char2>::ToString() const {
-  string s = "";
-  return s;
-}
-
-template <>
-/*virtual*/ bool ArrayKernelArgValue<cl_char2>::ElementEquality(
-    const cl_char2& lhs, const cl_char2& rhs) const {
-  return Vec2Equality(lhs, rhs);
->>>>>>> 8b16e8e86... Work in progress on cldrive vector args.:gpu/cldrive/array_kernel_arg_value.cc
-}
-=======
-namespace {}
->>>>>>> 36b52bcfa... Work in progress cldrive args.:gpu/cldrive/array_kernel_arg_value.cc
 
 }  // namespace cldrive
 }  // namespace gpu
 
 int main(int argc, char** argv) {
-  DCHECK(argc == 1) << "No command line arguments allowed";
+  CHECK(argc == 1) << "No command line arguments allowed";
 
   gpu::cldrive::CldriveInstances instances;
   CHECK(instances.ParseFromIstream(&std::cin));
