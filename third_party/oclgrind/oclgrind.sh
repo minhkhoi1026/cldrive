c1e2699922344f621f585ecd0a89add6cda3189c
Chris Cummins <chrisc.101@gmail.com>
2020-01-07 16:53:40 +0000

    Move //third_party:oclgrind into separate package.
    [Exported from 2695fd9df2c605e0f00f1ce057a54a2db11fe6cb]

--- third_party/oclgrind/oclgrind.sh
+++ third_party/oclgrind/oclgrind.sh
@@ -4,29 +4,14 @@
 #
 # Usage:
 #
-#    bazel run //third_party/oclgrind -- <oclgrind_args>
+#    bazel run //third_party:oclgrind -- <oclgrind_args>
 #
-
-# --- begin labm8 init ---
-f=phd/labm8/sh/app.sh
-source "${RUNFILES_DIR:-/dev/null}/$f" 2>/dev/null ||
-  source "$(grep -sm1 "^$f " "${RUNFILES_MANIFEST_FILE:-/dev/null}" | cut -f2- -d' ')" 2>/dev/null ||
-  source "$0.runfiles/$f" 2>/dev/null ||
-  source "$(grep -sm1 "^$f " "$0.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null ||
-  source "$(grep -sm1 "^$f " "$0.exe.runfiles_manifest" | cut -f2- -d' ')" 2>/dev/null ||
-  {
-    echo >&2 "ERROR: cannot find $f"
-    exit 1
-  }
-f=
-# --- end app init ---
-
-set -e
-if [[ -n $(DataPath oclgrind_linux/bin/oclgrind) ]]; then
-  $(DataPath oclgrind_linux/bin/oclgrind) "$@"
-elif [[ -n $(DataPath oclgrind_mac/bin/oclgrind) ]]; then
-  $(DataPath oclgrind_mac/bin/oclgrind) "$@"
+set -eu
+if [[ -f external/oclgrind_linux/bin/oclgrind ]]; then
+  external/oclgrind_linux/bin/oclgrind $@
+elif [[ -f external/oclgrind_mac/bin/oclgrind ]]; then
+  external/oclgrind_mac/bin/oclgrind $@
 else
-  echo "oclgrind not found!" >&2
+  echo "oclgrind not found!" >&2;
   exit 1
 fi
