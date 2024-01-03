c45092cd7e5119ce59eb98b36ad8a67643fd8d0e
minhkhoi1026 <minhkhoi1026@gmail.com>
2024-01-03 08:43:42 +0000

    add utility function for cldrive post-processing

--- run_cldrive.py
+++ run_cldrive.py
@@ -1,4 +1,5 @@
 import io
+from logging import config
 import pathlib
 import subprocess
 import tempfile
@@ -20,13 +21,15 @@ MEM_ANALYSIS_DIR = "mem-analysis"
 os.makedirs(BACKUP_DIR, exist_ok=True)
 TIMEOUT = 100
 NRUN = 10
-NUM_GPU = 2
+
+GPU_POOL = [1, 2]
+NUM_GPU = len(GPU_POOL)
 verbose_cldrive = False
 device_num_sm_dict = {
     "GPU|NVIDIA|NVIDIA_GeForce_RTX_3090|535.86.05|3.0": 82,
     "GPU|NVIDIA|NVIDIA_A10|535.104.05|3.0": 72
 }
-device_num_sm = 72
+device_num_sm = 80
 BACKUPED_LIST = set(list(os.path.splitext(path)[0] for path in os.listdir(BACKUP_DIR)))
 
 random.seed(2610)
@@ -241,11 +244,35 @@ def get_file_id_from_config(config):
     return f"{kernel_id}_{config['gsize']}_{config['lsize']}"
 
 def get_kernel_cldrive_df(config):
+    fileid = get_file_id_from_config(config)
+    
+    # Cannot run kernel if it contains printf, since the stdout will also contain printf results
+    # which will break the parsing of CLDrive output
     with open(config["kernel_path"], "r", encoding="utf-8") as f:
         src = f.read()
+        
+    if "printf" in src:
+        logger.debug(f"PRINTF_NOT_SUPPORTED {fileid}")
+        result = {
+            "kernel_path": config["kernel_path"],
+            "num_runs": config["num_runs"],
+            "gsize": config["gsize"],
+            "lsize": config["lsize"],
+            "kernel_name": "",
+            "outcome": "PRINTF_NOT_SUPPORTED",
+            "device_name": "",
+            "work_item_local_mem_size": 0,
+            "work_item_private_mem_size": 0,
+            "transferred_bytes": [],
+            "transfer_time_ns": [],
+            "kernel_time_ns": [],
+            "stderr": "PRINTF_NOT_SUPPORTED",
+            "args_info": "[]",
+        }
+        pd.DataFrame([result]).to_csv(os.path.join(BACKUP_DIR, f"{fileid}.csv"), index=None)
+        return result
 
     cl_platform = getOpenCLPlatforms()[0]
-    fileid = get_file_id_from_config(config)
 
     logger.debug(
         f"Running {fileid} with kernel {config['kernel_path']}, gsize {config['gsize']}, lsize {config['lsize']}"
@@ -335,14 +362,17 @@ def wrapping_func(config):
 
 
 def set_cuda_visible():
-    process_number = multiprocessing.current_process()._identity[0] - 1
+    process_number = GPU_POOL[multiprocessing.current_process()._identity[0] - 1]
     os.environ["CUDA_VISIBLE_DEVICES"] = str(process_number)
 
 
 if __name__ == "__main__":
     # Define the list of elements you want to process
+    selected_kernels = set(pd.read_csv("selected_kernels.csv")["kernel_path"].to_list())
     configs = list(get_config())
-    configs = [config for config in configs if get_file_id_from_config(config) not in BACKUPED_LIST]
+    configs = [config for config in configs 
+                if get_file_id_from_config(config) not in BACKUPED_LIST 
+                    and config["kernel_path"] in selected_kernels]
 
     # # Create a pool of 4 processes
     num_processes = NUM_GPU
